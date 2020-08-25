1bb9457580cd5f6c90562f615b2084e69f14ed4b
miftahfarhan13 <miftahfarhan20@gmail.com>
2020-08-25 17:04:01 +0700

    Updates

---
+++ precache-manifest.97f8b7af3a4e5c3f618958795ec19694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd5bf39654d69fe18be85d8eebe8467",
+    "url": "/movie-app-react/index.html"
+  },
+  {
+    "revision": "414b54e391b8007c49a9",
+    "url": "/movie-app-react/static/css/main.61534d38.chunk.css"
+  },
+  {
+    "revision": "d2c735bc52ad037808e3",
+    "url": "/movie-app-react/static/js/2.f6a9c88f.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/movie-app-react/static/js/2.f6a9c88f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "414b54e391b8007c49a9",
+    "url": "/movie-app-react/static/js/main.dbf82546.chunk.js"
+  },
+  {
+    "revision": "64e0037c206578654b3b",
+    "url": "/movie-app-react/static/js/runtime-main.c5c16297.js"
+  }
+]);
